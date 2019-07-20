03f73cc553ce6116ee3afcec34cbd73e30fc4df7
songzxDev <songzx_2326@163.com>
2019-07-20 16:35:01 +0800

    将原有的hello接口转移至新建的controller目录下，完成测试

--- src/main/java/org/songzx/firstdemo/controller/MyTestController.java
+++ src/main/java/org/songzx/firstdemo/controller/MyTestController.java
@@ -1,12 +1,13 @@
 package org.songzx.firstdemo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyTestController {
-    @RequestMapping(path="/hello/")
-    public String hello() {
-        return "Hello World! ";
+    @RequestMapping(path = "/hello/")
+    public String hello(@RequestParam(value = "username", required = false, defaultValue = "World") String username) {
+        return "Hello " + username;
     }
 }
